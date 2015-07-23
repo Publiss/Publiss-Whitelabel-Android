047db3edf35077b8100ab60404db66234cc4dc77
Lukas Bloder <lukas.bloder@bytepoets.com>
2015-07-23 10:36:37 +0200

    [PUBLISS-97] integrate barcode scanner

--- app/src/main/java/com/publiss/whitelabel/KioskActivity.java
+++ app/src/main/java/com/publiss/whitelabel/KioskActivity.java
@@ -3,19 +3,28 @@ package com.publiss.whitelabel;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import com.bytepoets.bpobarcodescanner.BarcodeScannerHandler;
+import com.bytepoets.bpobarcodescanner.ScannerResultInterface;
+import com.bytepoets.bpobarcodescanner.configuration.BarcodeScannerConfiguration;
+import com.bytepoets.bpobarcodescanner.configuration.BarcodeScannerUiConfiguration;
+import com.bytepoets.bpobarcodescanner.model.ScanResult;
+import com.google.zxing.BarcodeFormat;
 import com.publiss.core.PublissConfig;
+import com.publiss.core.ui.WebViewActivity;
 import com.publiss.core.ui.menu.LoginLogoutMenuItem;
 import com.publiss.core.ui.menu.MenuItem;
 import com.publiss.core.ui.menu.MenuItemCallbacks;
 import com.publiss.core.ui.menu.MenuItemManager;
-import com.publiss.core.ui.menu.MenuItemOpenHtmlCallback;
 import com.publiss.core.ui.menu.MenuItemOpenUrlCallback;
 
 import java.util.Locale;
 
 public class KioskActivity extends com.publiss.core.ui.KioskActivity {
+
+    private BarcodeScannerHandler barcodeScannerHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,6 +81,34 @@ public class KioskActivity extends com.publiss.core.ui.KioskActivity {
             }
         });
 
+        barcodeScannerHandler = new BarcodeScannerHandler();
+        barcodeScannerHandler.setScannerResultInterface(new ScannerResultInterface() {
+            @Override
+            public void onScannerResult(ScanResult scanResult) {
+                if (scanResult.getFormat() == BarcodeFormat.EAN_13) {
+                    String url = getResources().getString(R.string.ean13_url_pattern, scanResult.getText());
+                    openInAppBrowserWithUrl(url, "Shop");
+                }
+            }
+
+            @Override
+            public void onScannerCanceled() {
+
+            }
+        });
+
+        MenuItem scannerMenuItem = new MenuItem("scanner", false, new MenuItemCallbacks() {
+            @Override
+            public void menuItemSelected(MenuItem menuItem, Activity activity) {
+                BarcodeScannerConfiguration configuration = BarcodeScannerConfiguration.builder()
+                        .addAllowedFormat(BarcodeFormat.EAN_13)
+                        .useCustomizedDefaultUiForScanning(BarcodeScannerUiConfiguration.barcodeScannerUIConfiguration().withTitle(R.string.bporssreader_title_entry_detail).build())
+                        .build();
+                barcodeScannerHandler.startBarcodeScanner(KioskActivity.this,configuration);
+
+            }
+        });
+
         MenuItemManager.getInstance().clearMenuItems();
         if(!getResources().getString(R.string.bporssreader_rss_feed_url).isEmpty()) {
             MenuItemManager.getInstance().addMenuItem(rssMenuItem);
@@ -81,10 +118,26 @@ public class KioskActivity extends com.publiss.core.ui.KioskActivity {
             MenuItemManager.getInstance().addMenuItem(facebookFeedMenuItem);
         }
 
+
         MenuItemManager.getInstance().addMenuItem(homepageMenuItem);
         MenuItemManager.getInstance().addMenuItem(aboutMenuItem);
         MenuItemManager.getInstance().addMenuItem(contactMenuItem);
         MenuItemManager.getInstance().addMenuItem(loginLogoutMenuItem);
+        MenuItemManager.getInstance().addMenuItem(scannerMenuItem);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (!barcodeScannerHandler.onActivityResult(requestCode, resultCode, data)) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    private void openInAppBrowserWithUrl(String url, String title) {
+        Intent intent = new Intent(this, WebViewActivity.class);
+        intent.putExtra(WebViewActivity.ACTIVITY_TITLE_EXTRA, title);
+        intent.putExtra(WebViewActivity.ACTIVITY_URI_EXTRA, url);
+        intent.putExtra(WebViewActivity.ACTIVITY_WEBVIEW_BACK_AS_UP_EXTRA, false);
+        this.startActivity(intent);
+    }
 }
